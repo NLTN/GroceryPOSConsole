2076340dbad56e9c5c51ae1caacf033ce4ad1b09
Nguyen Nguyen <nltn@msn.com>
2017-09-25 13:29:56 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using GroceryPOS.Data.DB;
+using GroceryPOS.Data.BLL;
+
+namespace GroceryPOSConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add a product
+            AddSamepleProduct();
+
+            // Search
+            SearchForaProduct("apple");
+        }
+
+        static void AddSamepleProduct()
+        {
+            ProductBLL.Add("D003", "Coke", 20, "noimage.jpg");
+        }
+
+        static void SearchForaProduct(string text)
+        {
+            var items = ProductBLL.GetProductsBySearchString("fruit");
+            Console.WriteLine("Search for '{0}' - {1} items Found", text, items.Count());
+        }
+    }
+}
